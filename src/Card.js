c0b5e7a7beb2799e6c9aa7ae8376047d8a5b0f15
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-01-05 12:36:12 -0700

    component-setup

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="Card">
+        <h2>Card Test</h2>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+export default Card;
---
+++ src/Timer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+
+class Timer extends Component {
+  render() {
+    return (
+      <div className="Timer">
+        <h2>Timer</h2>
+      </div>
+    );
+  }
+}
+
+export default Timer
